dc6f8522c9339e29f983c5bd355f92b3cbc8c235
Caveman Fu <settinghead@gmail.com>
2012-01-27 12:20:21 -0500

    wexpression-backend clean setup with spring-flex 1.5 and spring framework 3.0

---
+++ wexpression-backend/src/main/java/com/settinghead/wexpression/server/model/Word.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.settinghead.wexpression.server.model;
+
+/**
+ * @author settinghead
+ *
+ */
+public class Word {
+	/**
+	 * @return the word
+	 */
+	public String getWord() {
+		return word;
+	}
+	/**
+	 * @param word the word to set
+	 */
+	public void setWord(String word) {
+		this.word = word;
+	}
+	/**
+	 * @return the weight
+	 */
+	public double getWeight() {
+		return weight;
+	}
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	private String word;
+	private double weight;
+}
