b503c37d2fbcface1dcb2ecaff89cc79f063d40c
Caveman Fu <settinghead@gmail.com>
2012-05-13 17:45:47 -0400

    SSL fix for unix as suggested in http://stackoverflow.com/questions/9756283/rails-3-omniauth-ssl-error

--- groffle/config/initializers/omniauth.rb
+++ groffle/config/initializers/omniauth.rb
@@ -2,13 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   fb_permissions = "offline_access, read_stream, publish_stream, user_notes, user_status, user_activities, user_likes, user_interests, user_hometown, user_location, email, user_checkins"
-  
-  if RbConfig::CONFIG["host_os"] =~ /mingw|mswin/
-    ca_file = File.expand_path Rails.root.join("config", "cacert.pem")
-    ssl_options = {}
-    ssl_options[:ca_path] = '/etc/ssl/certs' if Rails.env.staging?
-    ssl_options[:ca_file] = ca_file
-    provider :facebook, GRAPH_APP_ID, GRAPH_SECRET, {:scope => fb_permissions, :client_options => {:ssl => ssl_options}}
+  if RbConfig::CONFIG["host_os"] == /linux/
+    provider :facebook, GRAPH_APP_ID, GRAPH_SECRET, {:scope => fb_permissions, :client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
   else
     provider :facebook, GRAPH_APP_ID, GRAPH_SECRET, {:scope => fb_permissions}
   end
