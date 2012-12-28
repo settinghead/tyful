f0b289bb3d191e099636bb67b9862ae4f8db877e
Xiyang Chen <settinghead@gmail.com>
2012-12-27 23:39:04 -0500

    spiral nudger improvement

--- cpp/cpp/nudger/ColorMapSpiralNudger.h
+++ cpp/cpp/nudger/ColorMapSpiralNudger.h
@@ -25,38 +25,32 @@ private:
 		return lerp(min2, max2, val);
 	}
 public:
-    inline Placement* nudgeFor(EngineShape* shape, Placement* placement, int attempt, int totalPlannedAttempts){
-        attempt = ( attempt + placement->patch->getLastAttempt() + totalPlannedAttempts) % totalPlannedAttempts;
-        Patch* p= placement->patch;
-        
-        Placement* retPoint = (Placement*)malloc(sizeof(Placement));
+    inline virtual void nudgeFor(EngineShape* shape, Placement* origin, Placement& placement, int attemptNumber, int totalPlannedAttempts) {
+//        int attempt = ( attemptNumber + origin->patch->getLastAttempt() + totalPlannedAttempts) % totalPlannedAttempts;
+        Patch* p= origin->patch;
         
         
+        int attempt = ( attemptNumber + p->getLastAttempt() ) % totalPlannedAttempts;
         
-        attempt = ( attempt + p->getLastAttempt() ) % totalPlannedAttempts;
+        double factor = p->getWidth() < p->getHeight() ? p->getWidth() : p->getHeight();
+        factor = factor /2.0f;
+//			if (p->getLevel() == 0)
+//				factor /= 6;
+//			else if (p->getLevel() == 1)
+//				factor /= 2;
+//		else
+//			factor = 30;
+
+//		factor *= 6;
         
-        double factor = p->getWidth() < p->getHeight() ? p->getWidth() : p
-        ->getHeight();
-        factor/=3.6;
-        //			if (p.getLevel() == 0)
-        //				factor /= 6;
-        //			else if (p.getLevel() == 1)
-        //				factor /= 2;
-        //		} else
-        //			factor = 30;
-        
-        //		factor *= 6;
-        
-		double rad= powerMap(0.6, attempt, 0, totalPlannedAttempts, 1, factor);
-        
-        //		var thetaIncrement = powerMap(1, attempt, 0, totalPlannedAttempt, 0.5, 0.3);
-        //		var theta:Number= thetaIncrement * attempt;
-		double theta = (double) rand() / double(RAND_MAX) * TWO_PI;
+		double rad= powerMap(0.6f, attempt, 0, totalPlannedAttempts, 1, factor);
+        double thetaIncrement = powerMap(1, attempt, 0, totalPlannedAttempts, 0.5, 0.3);
+        double theta = thetaIncrement * attempt;
+//		double theta = (double) rand() / double(RAND_MAX) * TWO_PI;
         CartisianPoint location(cos(theta) * rad,sin(theta) * rad);
         
-        retPoint->location = location;
-        retPoint->patch = placement->patch;
-        return retPoint;
+        placement.location = location;
+        placement.patch = origin->patch;
     }
     
 
